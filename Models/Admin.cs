225fecc4a6b0de6304ce9835e9dd8b1d00fbd80e
程秀峰 <32472889@qq.com>
2023-11-22 12:22:26 +0800

    Add files via upload

---
+++ Models/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    /// <summary>
+    /// 管理员类
+    /// </summary>
+    [Serializable]
+    public class Admin
+    {
+        // 登录账号
+        public int LoginId { get; set; }
+        // 用户名
+        public string AdminName { get; set; }
+        // 登录密码
+        public string LoginPwd { get; set; }
+    }
+}
